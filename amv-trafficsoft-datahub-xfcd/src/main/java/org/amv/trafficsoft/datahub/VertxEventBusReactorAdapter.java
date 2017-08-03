dcc153bc42729683750ca570d7de9bf14171c710
Alois Leitner <alois.leitner@amv-networks.com>
2017-08-03 12:49:49 +0200

    move logic from XfcdEvents to parent class VertxEventBusReactorAdapter

---
+++ amv-trafficsoft-datahub-xfcd/src/main/java/org/amv/trafficsoft/datahub/VertxEventBusReactorAdapter.java
@@ -0,0 +1,58 @@
+package org.amv.trafficsoft.datahub;
+
+import io.vertx.core.Vertx;
+import io.vertx.core.eventbus.MessageConsumer;
+import io.vertx.core.eventbus.MessageProducer;
+import io.vertx.core.json.Json;
+import io.vertx.core.streams.Pump;
+import io.vertx.ext.reactivestreams.ReactiveReadStream;
+import io.vertx.ext.reactivestreams.ReactiveWriteStream;
+import org.reactivestreams.Publisher;
+import org.reactivestreams.Subscriber;
+import reactor.core.publisher.Flux;
+
+import static java.util.Objects.requireNonNull;
+
+public class VertxEventBusReactorAdapter<E> {
+    private final Vertx vertx;
+
+    public VertxEventBusReactorAdapter(Vertx vertx) {
+        this.vertx = requireNonNull(vertx);
+    }
+
+    public <T extends E> void publish(Class<T> clazz, Publisher<T> publisher) {
+        requireNonNull(clazz);
+        requireNonNull(publisher);
+
+        ReactiveReadStream<Object> rrs = ReactiveReadStream.readStream();
+
+        Flux.from(publisher)
+                .map(Json::encode)
+                .retry()
+                .subscribe(rrs);
+
+        MessageProducer<Object> messageProducer = vertx.eventBus().publisher(clazz.getName());
+
+        Pump pump = Pump.pump(rrs, messageProducer);
+
+        pump.start();
+    }
+
+    public <T extends E> void subscribe(Class<T> clazz, Subscriber<T> subscriber) {
+        requireNonNull(clazz);
+        requireNonNull(subscriber);
+
+        final MessageConsumer<String> consumer = vertx.eventBus().consumer(clazz.getName());
+
+        ReactiveWriteStream<String> rws = ReactiveWriteStream.writeStream(vertx);
+
+        Flux.from(rws)
+                .map(json -> Json.decodeValue(json, clazz))
+                .retry()
+                .subscribe(subscriber);
+
+        Pump pump = Pump.pump(consumer.bodyStream(), rws);
+
+        pump.start();
+    }
+}
--- amv-trafficsoft-datahub-xfcd/src/main/java/org/amv/trafficsoft/datahub/xfcd/XfcdEvents.java
+++ amv-trafficsoft-datahub-xfcd/src/main/java/org/amv/trafficsoft/datahub/xfcd/XfcdEvents.java
@@ -1,59 +1,12 @@
 package org.amv.trafficsoft.datahub.xfcd;
 
 import io.vertx.core.Vertx;
-import io.vertx.core.eventbus.MessageConsumer;
-import io.vertx.core.eventbus.MessageProducer;
-import io.vertx.core.json.Json;
-import io.vertx.core.streams.Pump;
-import io.vertx.ext.reactivestreams.ReactiveReadStream;
-import io.vertx.ext.reactivestreams.ReactiveWriteStream;
+import org.amv.trafficsoft.datahub.VertxEventBusReactorAdapter;
 import org.amv.trafficsoft.datahub.xfcd.event.XfcdEvent;
-import org.reactivestreams.Publisher;
-import org.reactivestreams.Subscriber;
-import reactor.core.publisher.Flux;
 
-import static java.util.Objects.requireNonNull;
-
-public class XfcdEvents {
-    private final Vertx vertx;
+public class XfcdEvents extends VertxEventBusReactorAdapter<XfcdEvent> {
 
     public XfcdEvents(Vertx vertx) {
-        this.vertx = requireNonNull(vertx);
-    }
-
-    public <T extends XfcdEvent> void publish(Class<T> clazz, Publisher<T> publisher) {
-        requireNonNull(clazz);
-        requireNonNull(publisher);
-
-        ReactiveReadStream<Object> rrs = ReactiveReadStream.readStream();
-
-        Flux.from(publisher)
-                .map(Json::encode)
-                .retry()
-                .subscribe(rrs);
-
-        MessageProducer<Object> messageProducer = vertx.eventBus().publisher(clazz.getName());
-
-        Pump pump = Pump.pump(rrs, messageProducer);
-
-        pump.start();
-    }
-
-    public <T extends XfcdEvent> void subscribe(Class<T> clazz, Subscriber<T> subscriber) {
-        requireNonNull(clazz);
-        requireNonNull(subscriber);
-
-        final MessageConsumer<String> consumer = vertx.eventBus().consumer(clazz.getName());
-
-        ReactiveWriteStream<String> rws = ReactiveWriteStream.writeStream(vertx);
-
-        Flux.from(rws)
-                .map(json -> Json.decodeValue(json, clazz))
-                .retry()
-                .subscribe(subscriber);
-
-        Pump pump = Pump.pump(consumer.bodyStream(), rws);
-
-        pump.start();
+        super(vertx);
     }
 }
