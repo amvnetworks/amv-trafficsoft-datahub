a58b94dffb0d70a24bb1582893f9a683afecebad
Alois Leitner <alois.leitner@amv-networks.com>
2017-09-08 08:44:27 +0200

    use blocking method in XfcdGetDataPublisherImpl instead of Observable to Flux conversion - its just simpler

--- amv-trafficsoft-datahub-xfcd/src/main/java/org/amv/trafficsoft/datahub/xfcd/XfcdGetDataPublisherImpl.java
+++ amv-trafficsoft-datahub-xfcd/src/main/java/org/amv/trafficsoft/datahub/xfcd/XfcdGetDataPublisherImpl.java
@@ -3,11 +3,13 @@ package org.amv.trafficsoft.datahub.xfcd;
 import lombok.Builder;
 import lombok.extern.slf4j.Slf4j;
 import org.amv.trafficsoft.rest.client.xfcd.XfcdClient;
+import org.amv.trafficsoft.rest.xfcd.model.DeliveryRestDto;
 import org.reactivestreams.Subscriber;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.FluxSink;
 
 import java.util.Collections;
+import java.util.List;
 import java.util.function.Consumer;
 
 @Slf4j
@@ -19,16 +21,23 @@ public class XfcdGetDataPublisherImpl implements XfcdGetDataPublisher {
 
     @Override
     public void subscribe(Subscriber<? super TrafficsoftDeliveryPackage> subscriber) {
-        Consumer<FluxSink<TrafficsoftDeliveryPackage>> fluxSinkConsumer = fluxSink -> xfcdClient
-                .getDataAndConfirmDeliveries(contractId, Collections.emptyList())
-                .toObservable()
-                .map(val -> TrafficsoftDeliveryPackageImpl.builder()
-                        .deliveries(val)
+        Consumer<FluxSink<TrafficsoftDeliveryPackage>> fluxSinkConsumer = fluxSink -> {
+            try {
+                List<DeliveryRestDto> deliveryRestDtos = xfcdClient
+                        .getDataAndConfirmDeliveries(contractId, Collections.emptyList())
+                        .execute();
+
+                TrafficsoftDeliveryPackage deliveryPackage = TrafficsoftDeliveryPackageImpl.builder()
+                        .deliveries(deliveryRestDtos)
                         .contractId(contractId)
-                        .build())
-                .subscribe(fluxSink::next,
-                        fluxSink::error,
-                        fluxSink::complete);
+                        .build();
+
+                fluxSink.next(deliveryPackage);
+                fluxSink.complete();
+            } catch (Exception e) {
+                fluxSink.error(e);
+            }
+        };
 
         Flux.create(fluxSinkConsumer)
                 .subscribe(subscriber);
