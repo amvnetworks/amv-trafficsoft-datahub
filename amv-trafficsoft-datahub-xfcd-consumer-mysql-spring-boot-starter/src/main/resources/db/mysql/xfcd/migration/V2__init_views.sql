46a2dc7fa7de6d9b06e38ba960815a72ffb4fea8
Alois Leitner <alois.leitner@amv-networks.com>
2017-09-12 17:04:45 +0200

    add views to migration scripts

---
+++ amv-trafficsoft-datahub-xfcd-consumer-mysql-spring-boot-starter/src/main/resources/db/mysql/xfcd/migration/V2__init_views.sql
@@ -0,0 +1,12 @@
+
+create view or replace `amv_trafficsoft_xfcd_node_with_states` as
+    select n.*, s.`CD` as `STATE_CD`, s.`VAL` as `STATE_VAL`
+    from `amv_trafficsoft_xfcd_node` n inner join `amv_trafficsoft_xfcd_state` s
+    on n.`ID` = s.`IMXFCD_N_ID`
+    order by n.`ID` asc, n.`TS` asc;
+
+create view or replace `amv_trafficsoft_xfcd_node_with_xfcds` as
+    select n.*, x.`TYPE` as `XFCD_TYPE`, x.`VAL` as `XFCD_VAL`, x.`VALSTR` as `XFCD_VALSTR`
+    from `amv_trafficsoft_xfcd_node` n inner join `amv_trafficsoft_xfcd_xfcd` x
+    on n.`ID` = x.`IMXFCD_N_ID`
+    order by n.`ID` asc, n.`TS` asc;
