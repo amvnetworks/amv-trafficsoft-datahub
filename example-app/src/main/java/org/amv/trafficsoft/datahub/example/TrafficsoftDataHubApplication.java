66ee3f58507209fcd3249026475cee009be46864
Thomas Jetzinger <thomas.jetzinger@amv-networks.com>
2022-08-02 15:49:01 +0200

    working on build

--- example-app/src/main/java/org/amv/trafficsoft/datahub/example/TrafficsoftDataHubApplication.java
+++ example-app/src/main/java/org/amv/trafficsoft/datahub/example/TrafficsoftDataHubApplication.java
@@ -2,10 +2,9 @@ package org.amv.trafficsoft.datahub.example;
 
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.Banner;
+import org.springframework.boot.WebApplicationType;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
-import org.springframework.boot.system.ApplicationPidFileWriter;
-import org.springframework.boot.system.EmbeddedServerPortFileWriter;
 
 @Slf4j
 @SpringBootApplication
@@ -15,17 +14,8 @@ public class TrafficsoftDataHubApplication {
         log.info("Starting {} ...", TrafficsoftDataHubApplication.class.getSimpleName());
 
         new SpringApplicationBuilder(TrafficsoftDataHubApplication.class)
-                .web(true)
+                .web(WebApplicationType.SERVLET)
                 .bannerMode(Banner.Mode.CONSOLE)
-                .listeners(applicationPidFileWriter(), embeddedServerPortFileWriter())
                 .run(args);
-    }
-
-    private static ApplicationPidFileWriter applicationPidFileWriter() {
-        return new ApplicationPidFileWriter("app.pid");
-    }
-
-    private static EmbeddedServerPortFileWriter embeddedServerPortFileWriter() {
-        return new EmbeddedServerPortFileWriter("app.port");
     }
 }
