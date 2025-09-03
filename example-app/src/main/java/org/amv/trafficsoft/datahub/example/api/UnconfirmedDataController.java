f973ed57d86bdf31d0bbe5691d62d03ce6d91b76
Thomas Jetzinger <thomas.jetzinger@amv-networks.com>
2025-09-03 14:40:19 +0200

    working on build

--- example-app/src/main/java/org/amv/trafficsoft/datahub/example/api/UnconfirmedDataController.java
+++ example-app/src/main/java/org/amv/trafficsoft/datahub/example/api/UnconfirmedDataController.java
@@ -1,9 +1,8 @@
 package org.amv.trafficsoft.datahub.example.api;
 
-import io.vertx.core.json.Json;
-import org.amv.trafficsoft.rest.client.autoconfigure.TrafficsoftApiRestProperties;
 import org.amv.trafficsoft.xfcd.consumer.jdbc.TrafficsoftDeliveryJdbcDao;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -14,16 +13,14 @@ import java.util.List;
 @RequestMapping("/data")
 public class UnconfirmedDataController {
 
-    @Autowired
-    TrafficsoftApiRestProperties apiRestProperties;
+    @Value("${amv.trafficsoft.rest.contract-id}")
+    private long contractId;
 
     @Autowired
     TrafficsoftDeliveryJdbcDao dao;
 
     @GetMapping("/unconfirmed")
-    public String index() {
-        final List<Long> idsOfUnconfirmedDeliveriesByBpcId = dao
-                .findIdsOfUnconfirmedDeliveriesByBpcId(apiRestProperties.getContractId());
-        return Json.encodePrettily(idsOfUnconfirmedDeliveriesByBpcId);
+    public List<Long> index() {
+        return dao.findIdsOfUnconfirmedDeliveriesByBpcId(contractId);
     }
 }
